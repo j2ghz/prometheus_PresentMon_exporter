8fc817947eadc40a958430ff6a7bd7ebe3e87892
J2ghz <j2.00ghz@gmail.com>
2019-06-15 20:09:52 +0200

    manual buckets

--- PresentMonToPrometheus/Program.cs
+++ PresentMonToPrometheus/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Prometheus.Client;
@@ -32,6 +33,28 @@ namespace PresentMonToPrometheus
             //"PresentMode"
         };
 
+        private static readonly double[] frametimesBuckets =
+        {
+            1,
+            5,
+            7,
+            10,
+            12.5,
+            15,
+            1000.0 / 60,
+            20,
+            25,
+            1000.0 / 30,
+            40,
+            50,
+            75,
+            100,
+            200,
+            1000
+        };
+
+        public static double[] FrametimesBuckets => frametimesBuckets.Select(d=>d*1000).ToArray();
+
         // 8 Dropped,
         private readonly CounterInt64 dropped = Metrics.CreateCounterInt64(prefix + "frames_dropped", "Number of dropped frames", true, labels);
 
@@ -40,22 +63,22 @@ namespace PresentMonToPrometheus
 
         //11 MsBetweenDisplayChange,
         private readonly Histogram msBetweenDisplayChange =
-            Metrics.CreateHistogram(prefix + "frametimes_display_between_ms", "MsBetweenDisplayChange", true, labels);
+            Metrics.CreateHistogram(prefix + "frametimes_display_between_ms", "MsBetweenDisplayChange", true,FrametimesBuckets, labels);
 
         //10 MsBetweenPresents,
         private readonly Histogram msBetweenPresents =
-            Metrics.CreateHistogram(prefix + "frametimes_present_between_ms", "MsBetweenPresents", true, labels);
+            Metrics.CreateHistogram(prefix + "frametimes_present_between_ms", "MsBetweenPresents", true, FrametimesBuckets, labels);
 
         //12 MsInPresentAPI,
-        private readonly Histogram msInPresentAPI = Metrics.CreateHistogram(prefix + "frametimes_present_API_time_ms", "MsInPresentAPI", true, labels);
+        private readonly Histogram msInPresentAPI = Metrics.CreateHistogram(prefix + "frametimes_present_API_time_ms", "MsInPresentAPI", true, FrametimesBuckets, labels);
 
         //14 MsUntilDisplayed
         private readonly Histogram msUntilDisplayed =
-            Metrics.CreateHistogram(prefix + "frametimes_display_delay_ms", "MsUntilDisplayed", true, labels);
+            Metrics.CreateHistogram(prefix + "frametimes_display_delay_ms", "MsUntilDisplayed", true, FrametimesBuckets, labels);
 
         //13 MsUntilRenderComplete,
         private readonly Histogram msUntilRenderComplete =
-            Metrics.CreateHistogram(prefix + "frametimes_render_time_ms", "MsUntilRenderComplete", true, labels);
+            Metrics.CreateHistogram(prefix + "frametimes_render_time_ms", "MsUntilRenderComplete", true, FrametimesBuckets, labels);
 
         // 9 TimeInSeconds,
         private readonly Gauge timeInSeconds = Metrics.CreateGauge(prefix + "raw_TimeInSeconds", "TimeInSeconds", true, labels);
